e527cbfa1720c738c31c0aa3c9621477d1a1d8ef
Munindra Zinzuvadia <munindrasoni99@gmail.com>
2022-08-14 18:50:23 -0400

    React app created

---
+++ client/src/App.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { makeStyles } from "@mui/styles";
+import "./App.css";
+
+const useStyles = makeStyles((theme) => ({}));
+function App() {
+  return (
+    <div>
+      <h1>ConvictXX'S Dream</h1>
+      <footer>
+        <a
+          href="https://www.flaticon.com/free-icons/quality"
+          title="quality icons"
+        >
+          Quality icons created by Freepik - Flaticon
+        </a>
+      </footer>
+    </div>
+  );
+}
+
+export default App;
